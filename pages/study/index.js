fd2fd3b8d375cb591de29ef2ae8566f332896636
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-01-01 21:42:05 +0200

    Elisei/create component

---
+++ pages/study/index.js
@@ -0,0 +1,11 @@
+import Layout from 'components/layout'
+import styles from "./study.module.scss"
+
+export default function Page() {
+  return (
+    <Layout>
+      <>
+      </>
+    </Layout>
+  )
+}
