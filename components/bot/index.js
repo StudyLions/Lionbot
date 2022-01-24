e4a94eabf89c516fa634391ef1f1910bd2c4e30a
Stefan Stax Miljkovic <49881085+stefanstax@users.noreply.github.com>
2022-01-24 09:49:24 +0100

    + Adequate blocks added for all elments on the page | + Spacing fixed… (#11)
    * + Adequate blocks added for all elments on the page | + Spacing fixed for elements | + Invite bot block layout change
    
    * + Comment review patches | + editorConfig | + Added priority for the image above the fold to load instantly instead of lazy ( SEO + Accessibility practises )
    
    Co-authored-by: Stefan Stax <stax@stefanstax.com>

--- components/bot/index.js
+++ components/bot/index.js
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import styles from "./index.module.scss"
-import {default as cards} from './bot-cards-info.json'
-import Layout from 'components/layout'
+import Image from 'next/image';
+import styles from './index.module.scss';
+import {default as cards} from './bot-cards-info.json';
+import Layout from 'components/layout';
 
 export default function Page() {
   return (
@@ -9,51 +9,67 @@ export default function Page() {
       <div>
         <div className={styles.container_card}>
           <div className={`row flex-row ${styles.card}`}>
-            <div className="col-md-6 col-sm-12">
-              <div className={styles.title_blue}>Build the best Discord study community</div>
+            <div className="col-12 col-xl-6">
+              <div className={styles.title_blue}>
+                <h1>Build the best Discord study community with StudyLion</h1>{" "}
+              </div>
 
               <div className={styles.description_white}>
-                <b>StudyLion</b> is the best, <b>open-source</b>, all-in-one solution that <b>every study community
-                needs.</b>
+                <p>
+                  <b>StudyLion</b> is the best, <b>open-source</b>, all-in-one solution that{" "}
+                  <b>every study community needs.</b>
+                </p>
               </div>
               <div className={`fst-italic ${styles.description_white}`}>
-                Learn how to create a Discord co-working environment for your classmates or coworkers!
+                <p>Learn how to create a Discord co-working environment for your classmates or coworkers!</p>
               </div>
               <div className={styles.description_white}>
-                Start <a className={`text-decoration-none ${styles.link}`} href={'https://discord.studylions.com/invite'}
-                         target={'_blank'}>inviting
-                our StudyLion bot</a> to your Discord community and configuring it using our tutorials.
+                <p>
+                  <a className={`${styles.link}`} href={"https://discord.studylions.com/invite"}>
+                    StudyLion Bot
+                  </a>{" "}
+                  in your Discord community for free with configuration tutorials? Yeap! For Free. Always.
+                </p>
+                <a
+                  className={`d-flex justify-content-center text-decoration-none text-uppercase ${styles.link} ${styles.buttonLink}`}
+                  href={"https://discord.studylions.com/invite"}
+                  target={"_blank"}
+                >
+                  Claim StudyLion for Free!
+                </a>
               </div>
             </div>
-            <div className={`col-md-6 col-sm-12 ${styles.image_card}`}>
-              <Image
-                src={require('public/images/banner-1.png')}
-                alt="Study bot lion discord logo"
-              />
+            <div className={`col-12 col-xl-6 ${styles.image_card}`}>
+              <Image priority src={require("public/images/banner-1.png")} alt="Study bot lion discord logo" />
             </div>
           </div>
         </div>
 
-        <div className={`text-center ${styles.trusted_by_2500}`}>Trusted by 2,500+ Study Servers</div>
+        <div className={`text-center ${styles.trusted_by_2500}`}>
+          <h2>Trusted by 2,500+ Study Servers</h2>
+        </div>
 
         {cards.map((card, i) => (
           <div className={styles.container_card} key={card.title + i}>
             <div className={`row ${styles.card}`}>
-              <div className="col-md-6 col-sm-12">
-                <div className={styles.title_blue}>{card.title}</div>
-                <div className={styles.description_white}>{card.description_1}</div>
-                <div className={`${styles.description_white}`}>{card.description_2}</div>
+              <div className="col-12 col-lg-6 d-flex flex-column align-items-start justify-content-center">
+                <div className={styles.title_blue}>
+                  <h3>{card.title}</h3>
+                </div>
+                <div className={styles.description_white}>
+                  <p>{card.description_1}</p>
+                </div>
+                <div className={`${styles.description_white}`}>
+                  <p>{card.description_2}</p>
+                </div>
               </div>
-              <div className={`col-md-6 col-sm-12 ${styles.image_card}`}>
-                <Image
-                  src={require(`public/images/${card.image.src}`)}
-                  alt={card.image.alt}
-                />
+              <div className={`col-12 col-lg-6 my-5 md-my-0 ${styles.image_card}`}>
+                <Image src={require(`public/images/${card.image.src}`)} alt={card.image.alt} />
               </div>
             </div>
           </div>
         ))}
       </div>
     </Layout>
-  )
+  );
 }
