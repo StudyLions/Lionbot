2f699b7affd44f53f311103aff4b06de26fc78ac
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-12 21:22:13 +0200

    DS-29/ Lion gems section

--- constants/DonationsData.ts
+++ constants/DonationsData.ts
@@ -1,40 +1,38 @@
-const defaultDonationImage = "/../public/images/StudyLion_1.png"
-
 export const DonationsData = [
   {
     amount: 0.99,
     tokens: 300,
     tokens_bonus: 0,
-    image: defaultDonationImage
+    image: 'https://cdn.discord.study/images/liongems_1.png'
   },
   {
     amount: 4.99,
     tokens: 1500,
     tokens_bonus: 50,
-    image: defaultDonationImage
+    image: 'https://cdn.discord.study/images/liongems_2.png'
   },
   {
     amount: 14.99,
     tokens: 4500,
     tokens_bonus: 600,
-    image: defaultDonationImage
+    image: 'https://cdn.discord.study/images/liongems_3.png'
   },
   {
     amount: 29.99,
     tokens: 9000,
     tokens_bonus: 1500,
-    image: defaultDonationImage
+    image: 'https://cdn.discord.study/images/liongems_4.png'
   },
   {
     amount: 49.99,
     tokens: 15000,
     tokens_bonus: 3000,
-    image: defaultDonationImage
+    image: 'https://cdn.discord.study/images/liongems_5.png'
   },
   {
     amount: 99.99,
     tokens: 30000,
     tokens_bonus: 7000,
-    image: defaultDonationImage
+    image: 'https://cdn.discord.study/images/liongems_6.png'
   },
 ]
--- pages/liongems.tsx
+++ pages/liongems.tsx
@@ -1,39 +1,11 @@
 import Layout from "@/components/Layout/Layout";
-import { loadStripe } from "@stripe/stripe-js";
-import { DonationsData } from "constants/DonationsData";
-import DonationCard from "@/components/DonationCard/DonationCard";
 import HeaderContentLionGems from "@/components/liongems/HeaderContentLionGems";
+import MiddleSectionLionGems from "@/components/liongems/MiddleSectionLionGems";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
 export default function Liongems() {
-
-  const createPaymentSession = async (price: number) => {
-    // Create Checkout session on backend
-    const { sessionId } = await fetch('/api/checkout/session', {
-      method: 'POST',
-      headers: {
-        'content-type': "application/json",
-      },
-      body: JSON.stringify({ quantity: 1, amount: price })
-    }).then(res => res.json());
-
-    //When the customer clicks on the button, redirect them to Checkout.
-    const stripe = await stripePromise;
-    const { error } = await stripe.redirectToCheckout({
-      sessionId,
-    });
-  }
-
   return <Layout>
     <HeaderContentLionGems />
-    <div className={ `container` }>
-      <h1 className={ `mb-5 text-center` }>Support Discord.study Development</h1>
-      <div style={ { display: "grid", gridTemplateColumns: "repeat(3,1fr)", gap: "30px", justifyItems: 'center' } }>
-        { DonationsData.map((product, index) => (
-                <DonationCard onSelect={ createPaymentSession } { ...product } key={ index }/>
-        )) }
-      </div>
-    </div>
+    <MiddleSectionLionGems />
   </Layout>
 }
