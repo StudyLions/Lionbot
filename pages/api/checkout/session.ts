64dce5d5fbc318015b3951bb83d460293f45d02f
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-12 18:58:06 +0200

    created HeaderContentLionGems component.

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -29,7 +29,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }],
     mode: 'payment',
     success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${req.headers.origin + NavigationPaths.SupportUS}`,
+    cancel_url: `${req.headers.origin + NavigationPaths.lionGems}`,
   })
   res.status(200).json({ sessionId: session.id })
 }
