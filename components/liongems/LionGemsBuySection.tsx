e96168faf22a85444fb99f889e4524cd15dfe3be
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-13 22:37:18 +0200

    Elisei/DS-32/create FAQ section

--- components/liongems/LionGemsBuySection.tsx
+++ components/liongems/LionGemsBuySection.tsx
@@ -1,7 +1,6 @@
 import { DonationsData } from "@/constants/DonationsData";
 import { loadStripe } from "@stripe/stripe-js";
 import styles from './Liongems.module.scss'
-import Image from "next/image";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
@@ -24,17 +23,12 @@ export default function LionGemsBuySection() {
   }
 
   return <div className={ `${ styles.buySection }` }>
-    <h1 className={ `uppercase text-center mt-28 mb-20 font-bold text-7xl ${ styles.buySectionTitle }` }>Lion gems</h1>
+    <h1 className={ `uppercase text-center mt-28 mb-20 font-bold text-7xl ${ styles.title }` }>Lion gems</h1>
     <div className={ 'grid grid-cols-3 xl:gap-x-32 lg:gap-x-20 gap-y-10 items-center place-items-center' }>
       { DonationsData.map((product, index) => (
               <div className={ `rounded-3xl pt-3 h-fit flex flex-col w-full ${ styles.donationCard }` }
                    key={ product.amount + index }>
-                <Image src={ product.image }
-                       alt={ `Tokens ${ product.tokens } image` }
-                       width={ 240 }
-                       height={ 200 }
-                       objectFit={ "contain" }
-                />
+                <img src={ product.image } alt={ `Tokens ${ product.tokens } image` } loading={"lazy"}/>
                 <p className={ `text-4xl font-bold text-center ${ styles.gems }` }>{ product.tokens }</p>
                 <p className={ `text-2xl font-bold text-center mb-5 
                                 ${ product.tokens_bonus ? styles.gemsBonus : 'text-transparent' }` }>
