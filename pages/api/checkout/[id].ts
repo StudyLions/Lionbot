da30e066289cfc40e076e7cce520a3de6a668a53
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-14 21:14:38 +0200

    make stripe secret string

--- pages/api/checkout/[id].ts
+++ pages/api/checkout/[id].ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
   apiVersion: '2020-08-27',
 })
 
