7c76c10ce8d85a3f23a9b722ff26534bacd7149b
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-19 16:07:14 +0200

    fix undefined .env var bug (Stripe public key)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  images: {
-    domains: ['cdn.discord.study', 'api-production.s3.amazonaws.com'],
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
   },
+  images: {
+    domains: ['cdn.discord.study', 'api-production.s3.amazonaws.com']
+  }
 }
