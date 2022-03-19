eebcb4788e912ab65168f8479ee588e772aa56c7
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-19 19:31:21 +0200

    rm useless code.

--- components/liongems/LionGemsBuySection.tsx
+++ components/liongems/LionGemsBuySection.tsx
@@ -23,7 +23,6 @@ export default function LionGemsBuySection() {
   }
 
   return <div className={ `${ styles.buySection }` }>
-    STRIPE_PUBLISHABLE_KEY = {process.env.STRIPE_PUBLISHABLE_KEY}
     <h1 className={ `uppercase text-center mt-28 mb-20 font-bold text-7xl ${ styles.title }` }>Lion gems</h1>
     <div className={ 'grid grid-cols-3 xl:gap-x-32 lg:gap-x-20 gap-y-10 items-center place-items-center' }>
       { DonationsData.map((product, index) => (
