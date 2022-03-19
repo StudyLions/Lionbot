c202656678d3af9fe6a9419ce8db8816fef9252e
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-03-19 19:49:03 +0200

    remove useless code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-  },
   images: {
     domains: ['cdn.discord.study', 'api-production.s3.amazonaws.com']
   }
