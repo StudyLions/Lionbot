798fffc1b867966c9105ab12b9cad3fe7f0655e7
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-16 12:13:51 +0300

    reformat code.

--- components/liongems/LionGemsBuySection.tsx
+++ components/liongems/LionGemsBuySection.tsx
@@ -1,25 +1,24 @@
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import {signIn, useSession} from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { signIn, useSession } from "next-auth/react";
 
-import {AmountModal} from "@/components/AmountModal";
-import {PurchaseFailedModal} from "@/components/PurchaseFailedModal";
-import {PurchaseCompleteModal} from "@/components/PurchaseCompleteModal";
-import {DonationsData} from "@/constants/DonationsData";
+import { AmountModal } from "@/components/AmountModal";
+import { PurchaseFailedModal } from "@/components/PurchaseFailedModal";
+import { PurchaseCompleteModal } from "@/components/PurchaseCompleteModal";
+import { DonationsData } from "@/constants/DonationsData";
 import numberWithCommas from "@/utils/numberWithCommas";
-import {IDonationItem} from "@/models/donationData";
+import { IDonationItem } from "@/models/donation";
 import styles from "./Liongems.module.scss";
 
-
 export default function LionGemsBuySection() {
-  const {data: session} = useSession()
+  const { data: session } = useSession();
   const [isAmountModalVisible, setIsAmountModalVisible] = useState(false);
   const [isPurchaseFailedVisible, setIsPurchaseFailedVisible] = useState(false);
   const [isPurchaseCompleteVisible, setIsPurchaseCompleteVisible] = useState(false);
-  const [currentDonationData, setCurrentDonationData] = useState(null)
+  const [currentDonationData, setCurrentDonationData] = useState(null);
   const router = useRouter();
 
-  useEffect((() => {
+  useEffect(() => {
     switch (router.query.payment) {
       case "success":
         setIsPurchaseCompleteVisible(true);
@@ -29,8 +28,8 @@ export default function LionGemsBuySection() {
         break;
       default:
     }
-    router.replace("/liongems", undefined, {shallow: true});
-  }), [router.query.payment])
+    router.replace("/liongems", undefined, { shallow: true });
+  }, [router.query.payment]);
 
   const openAmountModal = (donationItem: IDonationItem) => {
     //Redirect user if is not logged.
@@ -38,46 +37,60 @@ export default function LionGemsBuySection() {
       signIn("discord");
       return;
     }
-    setCurrentDonationData(donationItem)
-    setIsAmountModalVisible(true)
-  }
+    setCurrentDonationData(donationItem);
+    setIsAmountModalVisible(true);
+  };
 
   return (
-          <>
-            {isAmountModalVisible && <AmountModal
-                    closeModal={() => setIsAmountModalVisible(false)} {...currentDonationData}/>}
-            {isPurchaseFailedVisible && <PurchaseFailedModal
-                    closeModal={() => {
-                      setIsPurchaseFailedVisible(false);
-                      localStorage.removeItem('basket')
-                    }}/>}
-            {isPurchaseCompleteVisible && <PurchaseCompleteModal
-                    closeModal={() => {
-                      setIsPurchaseCompleteVisible(false);
-                      localStorage.removeItem('basket');
-                    }}/>}
-            
-            <div className={`${styles.buySection}`}>
-              <h1 className={`uppercase text-center mt-28 mb-20 font-bold text-7xl ${styles.title}`}>Lion gems</h1>
-              <div className={"grid grid-cols-3 xl:gap-x-32 lg:gap-x-20 gap-y-10 items-center place-items-center"}>
-                {DonationsData.map((donationItem: IDonationItem, index) => (
-                        <div className={`rounded-3xl pt-3 h-fit flex flex-col w-full ${styles.donationCard}`}
-                             key={donationItem.amount + index}>
-                          <img src={donationItem.image} alt={`Tokens ${donationItem.tokens} image`} loading={"lazy"}/>
-                          <p className={`text-4xl font-bold text-center ${styles.gems}`}>{numberWithCommas(donationItem.tokens)}</p>
-                          <p className={`text-2xl font-bold text-center mb-5 ${+donationItem.tokens_bonus ? styles.gemsBonus : "text-transparent"}`}>
-                            +{numberWithCommas(donationItem.tokens_bonus)} bonus
-                          </p>
-                          <a onClick={() => openAmountModal(donationItem)}
-                             className={`rounded-full block text-2xl mx-5 mb-5 text-center py-1 font-bold cursor-pointer
+    <>
+      {isAmountModalVisible && (
+        <AmountModal closeModal={() => setIsAmountModalVisible(false)} {...currentDonationData} />
+      )}
+      {isPurchaseFailedVisible && (
+        <PurchaseFailedModal
+          closeModal={() => {
+            setIsPurchaseFailedVisible(false);
+            localStorage.removeItem("basket");
+          }}
+        />
+      )}
+      {isPurchaseCompleteVisible && (
+        <PurchaseCompleteModal
+          closeModal={() => {
+            setIsPurchaseCompleteVisible(false);
+            localStorage.removeItem("basket");
+          }}
+        />
+      )}
+
+      <div className={`${styles.buySection}`}>
+        <h1 className={`uppercase text-center mt-28 mb-20 font-bold text-7xl ${styles.title}`}>Lion gems</h1>
+        <div className={"grid grid-cols-3 xl:gap-x-32 lg:gap-x-20 gap-y-10 items-center place-items-center"}>
+          {DonationsData.map((donationItem: IDonationItem, index) => (
+            <div
+              className={`rounded-3xl pt-3 h-fit flex flex-col w-full ${styles.donationCard}`}
+              key={donationItem.amount + index}
+            >
+              <img src={donationItem.image} alt={`Tokens ${donationItem.tokens} image`} loading={"lazy"} />
+              <p className={`text-4xl font-bold text-center ${styles.gems}`}>{numberWithCommas(donationItem.tokens)}</p>
+              <p
+                className={`text-2xl font-bold text-center mb-5 ${
+                  +donationItem.tokens_bonus ? styles.gemsBonus : "text-transparent"
+                }`}
+              >
+                +{numberWithCommas(donationItem.tokens_bonus)} bonus
+              </p>
+              <a
+                onClick={() => openAmountModal(donationItem)}
+                className={`rounded-full block text-2xl mx-5 mb-5 text-center py-1 font-bold cursor-pointer
                        bg-red20 hover:bg-red04`}
-                          >
-                            €{donationItem.amount}
-                          </a>
-                        </div>
-                ))}
-              </div>
+              >
+                €{donationItem.amount}
+              </a>
             </div>
-          </>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
