52db7cab214343157849071664ac80816490cfdc
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-16 12:14:44 +0300

    Premium plans : Create component - part1

---
+++ components/liongems/LionGemsPremiumPlan.tsx
@@ -0,0 +1,61 @@
+import { IPremiumPlan } from "@/models/premiumPlan";
+import styles from "./Liongems.module.scss";
+import numberWithCommas from "@/utils/numberWithCommas";
+let PremiumCards: Array<IPremiumPlan> = [
+  {
+    lionGems: 4000,
+    typeSubscription: "3 Months",
+  },
+  {
+    lionGems: 12000,
+    typeSubscription: "1 Year",
+  },
+  {
+    lionGems: 1500,
+    typeSubscription: "Monthly",
+  },
+];
+
+const PremiumCard = (card) => {
+  return (
+    <div className={"min-w-[364px] bg-white rounded-[30px]"}>
+      <div className={`alignCenter min-h-[130px] rounded-t-[30px] ${styles.premiumPlan_gradient}`}>
+        <div className={"font-semibold text-[50px] leading-[42px]"}>{numberWithCommas(card.lionGems)}</div>
+      </div>
+      <div className={"alignCenter mt-[30px] text-[40px] leading-[42px] text-[#333030] uppercase"}>
+        {card.typeSubscription}
+      </div>
+      <div className={"alignCenter mt-[20px] text-[#989595] text-[25px] mb-[30px] font-extralight"}>Subscription</div>
+    </div>
+  );
+};
+
+const LionGemsPremiumPlan = () => {
+  return (
+    <div className={"flex flex-col py-[150px] mx-auto"}>
+      <h1 className={"font-bold text-[70px] landing-[66px] uppercase text-center"}>Premium Plans</h1>
+      <p className={"text-center mt-[20px] text-[#B6B6B6] text-[20px] leading-[42px]"}>
+        You can use LionGems to purchase Premium subscirption for your server!{" "}
+      </p>
+      <div className={"alignCenter gap-[40px] mt-[20px]"}>
+        {PremiumCards.map((card: IPremiumPlan) => (
+          <PremiumCard key={card.typeSubscription} {...card} />
+        ))}
+      </div>
+      <div className={"alignCenter flex-col"}>
+        <p className={"mt-[30px] max-w-[550px] text-center mt-[20px] text-[#B6B6B6] text-[20px] leading-[42px]"}>
+          To purchase and manage your subscription please use the “premium” command and access the interface.
+        </p>
+        <div
+          className={
+            "mt-[20px] bg-white text-[#BD4949] py-[10px] px-[30px] rounded-full font-bold text-[20px] leading-[42px] uppercase"
+          }
+        >
+          Get LionGems
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LionGemsPremiumPlan;
--- pages/liongems.tsx
+++ pages/liongems.tsx
@@ -3,12 +3,14 @@ import LionGemsHeader from "@/components/liongems/LionGemsHeader";
 import LionGemsBuySection from "@/components/liongems/LionGemsBuySection";
 import LionGemsFAQSection from "@/components/liongems/LionGemsFAQSection";
 import LionGemsPerks from "@/components/liongems/LionGemsPerks";
+import LionGemsPremiumPlan from "@/components/liongems/LionGemsPremiumPlan";
 
 export default function Liongems() {
   return (
     <Layout>
       <LionGemsHeader />
       <LionGemsPerks />
+      <LionGemsPremiumPlan />
       <LionGemsBuySection />
       <LionGemsFAQSection />
     </Layout>
