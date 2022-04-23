3a030d4ca2d3883323bbdce145c0b167ff846dbe
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-23 16:48:42 +0300

    Create comingSoon page.

---
+++ pages/comingSoon.tsx
@@ -0,0 +1,16 @@
+import Layout from "@/components/Layout/Layout";
+
+export default function CommingSoon() {
+  return (
+    <>
+      <Layout>
+        <div className={"alignCenter flex-col h-[57vh] bg-gradient-to-r from-[#627AF7] to-[#EF566A] px-[30px]"}>
+          <h1 className={"text-[65px] font-bold uppercase md:text-[30px]"}>coming soon!</h1>
+          <h2 className={"mt-[10px] text-[20px] md:text-[18px] text-center"}>
+            We are currently creating something great.
+          </h2>
+        </div>
+      </Layout>
+    </>
+  );
+}
