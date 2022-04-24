a6be130946ef76c8938db57950c278cd29696a83
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-24 22:06:41 +0300

    add favicon.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,7 +7,10 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>StudyBot | title</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
         <meta name="description" content="meta description/" />
         <meta itemProp="name" content="itemprop meta name" />
         <meta itemProp="description" content="itemprop meta description" />
