aaa45b2f46e1164b9965c27e3fb5744f4a246759
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-25 08:49:59 +0300

    fix favicon issue.

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -11,8 +11,10 @@ export default function Layout({ children, SEO }: ILayout) {
         <meta name="description" content={SEO.description} />
         <meta itemProp="name" content={SEO.title + " | StudyBot"} />
         <meta itemProp="description" content={SEO.description} />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="public/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="public/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="public/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="public/favicon-16x16.png" />
       </Head>
       <Header />
       <main>{children}</main>
