62b8ba7b39d7b33a405283b18a67e6136f8bf977
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-25 18:35:43 +0300

    Homepage: fix cards paddings.

--- components/Homepage_content/HomepageSections.js
+++ components/Homepage_content/HomepageSections.js
@@ -12,7 +12,7 @@ export default function HomepageSections() {
           </div>
           <div className={styles.description_white} dangerouslySetInnerHTML={{ __html: card.description }}></div>
         </div>
-        <div className={"relative h-[400px] w-[60%]  lg:w-[calc(100%-30px)] lg:px-[30px]"}>
+        <div className={"relative h-[400px] w-[60%]  lg:w-[calc(100%-30px)] lg:px-[30px] md:h-[300px]"}>
           <Image className={styles.card_image} src={card.image.src} alt={card.image.alt} layout={"fill"} />
         </div>
       </div>
