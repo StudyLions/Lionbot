47104eeb09e8930816937c31e93441b0a5d2541e
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-04-25 14:17:01 +0300

    cleanup

--- constants/types.ts
+++
@@ -1,4 +0,0 @@
-export enum NavigationPaths {
-  Home= '/',
-  SupportUS = '/supportUS'
-}
--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  images: {
-    domains: ['api-production.s3.amazonaws.com'],
-  },
-}
