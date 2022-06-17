ae73ca425aa1be626201a6df8fa5eddfa7b3d023
Stefan Stäx <stefanstax@Stefans-MacBook-Air-2.local>
2022-06-17 10:36:25 +0200

    Refund policy added linking to TOS

--- constants/Footer.ts
+++ constants/Footer.ts
@@ -63,7 +63,7 @@ export const LegalItems = [
   {
     title: "Refund Policy",
     link: {
-      href: "/coming-soon",
+      href: NavigationPaths.termsAndConditions,
       target: "",
     },
   },
